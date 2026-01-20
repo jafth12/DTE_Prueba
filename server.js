17dc5d570cc2ef5beed9279b36d5eb612a70f2e3
jafth12 <maylicc68@gmail.com>
2026-01-19 21:15:46 -0600

    revision_DOS

--- server.js
+++ server.js
@@ -17,15 +17,6 @@ app.get('/', (req, res) => {
     res.send('API de Facturacion Electronica funcionando');
 });
 
-db.query('SELECT 1 + 1 AS solution')
-.then(([rows, fields]) => {
-    console.log('!La base de datos respondio el resultado es:', rows[0].solution);
-})
-.catch(err => {
-    console.error('ERROR GRAVE: No pudo conectar a la DB')
-    console.error('Revisa tu archivo .env detalle del error:', err.message);
-});
-
 app.listen(PORT, () => {
     console.log(`Servidor corriendo en http://localhost:${PORT}`);
 });
